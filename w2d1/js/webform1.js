2a9a3666c594bfc6c0d1d9cf40c6d377123c50d2
paudelnavin <navinpaudel@outlook.com>
2020-06-29 22:46:01 -0500

    w2d1 assignment

--- w2d1/js/webform1.js
+++ w2d1/js/webform1.js
@@ -1,7 +1,7 @@
 window.onload = function () {
     "use script"
     document.getElementById('submit').onclick = function () {
-        let username = document.getElementById('email').value;
+        let username = document.getElementById('emailid').value;
         // regex = new RegExp("E00", "i");
         let regex = /^([a-zA-Z0-9\._-]+)@([a-zA-Z0-9-]+).([a-z]{2,8})(.[a-z]{2,8})?$/;
 
