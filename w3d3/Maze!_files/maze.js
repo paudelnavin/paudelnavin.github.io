7743b3a906cc141c877aefb6e42abe528487d7d0
paudelnavin <navinpaudel@outlook.com>
2020-07-08 16:18:59 -0500

    w3d3 assignment

---
+++ w3d3/Maze!_files/maze.js
@@ -0,0 +1,51 @@
+/* jshint esversion: 6 */
+$(document).ready(function(){
+    "use strict";
+
+    let $boundary = $(".boundary");
+    let $maze = $("#maze");
+    let $status = $("#status");
+    let $start = $("#start");
+    let $end = $("#end");
+
+    $boundary.mouseenter(function() {
+        lostMessage();
+    });
+    
+    $maze.mouseleave(function () {
+        lostMessage();
+    });
+
+    let lostMessage = function(){
+        if (!$boundary.hasClass("youlose")){
+            $boundary.addClass("youlose");
+            if ($boundary.hasClass("started")){
+                $boundary.removeClass("started");
+            }
+            updateStatus("Sorry, you lost. 😞");
+        }
+    }
+
+    let updateStatus = function(msg){
+        $status.text(msg);
+    }
+
+    $end.mouseenter(function () {
+        if (!$boundary.hasClass("youlose")){
+            if ($boundary.hasClass("started")){
+                updateStatus("You win! 😀");
+            }
+        }
+    });
+
+    $start.click(function () {
+        if ($boundary.hasClass("youlose")){
+            $boundary.removeClass("youlose");
+            if (!$boundary.hasClass("started")){
+                $boundary.addClass("started");
+            }
+            updateStatus("You are playing now.");
+        }
+    });
+
+});
